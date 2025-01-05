46376b9762236ce6c0c24f74fe5bdc542a9a558e
Zhdanova Ekaterina <zhdanova.ekaterina.95@yandex.ru>
2025-01-05 16:39:31 +0300

    Tests: Разделение тестов базы на несколько файлов

--- tests/db/test_db_caster.py
+++ tests/db/test_db_caster.py
@@ -1,5 +1,5 @@
 """
-Тесты для коннектора к реляционной БД.
+Тесты для коннектора к реляционной БД (персонажи).
 Закрывают функционал выборок с помощью ORM.
 Для тестирования используется in-memory sqlite
 """
@@ -8,38 +8,14 @@ import pytest
 
 from app.core import NotFoundException
 from app.core.models.caster import Caster as CoreCaster
-from app.core.interfaces.dto import GameClassInfo
 from app.repository.db.models import Caster as DbCaster, CasterClass as DbCasterClass
-from tests.db.params import game_class_result
-
-
-@pytest.mark.dependency(name="one_class")
-def test_one_class(full_db):
-    first_class = game_class_result[0]
-
-    one_class = full_db.get_one_class(first_class.get('alias'))
-    assert type(one_class) is GameClassInfo
-    assert one_class.alias == first_class.get('alias')
-    assert (one_class.choose_subclass_level
-            == first_class.get('choose_subclass_level'))
-    assert len(one_class.subclasses) == len(first_class.get('subclasses'))
-
-
-def test_get_non_existing_class(full_db):
-    with pytest.raises(KeyError):
-        full_db.get_one_class('wizarddd')
-
-
-@pytest.mark.dependency(depends=["one_class"])
-def test_get_all_classes(full_db):
-    all_classes = full_db.get_all_classes()
-    assert len(all_classes) == len(game_class_result)
-    assert type(all_classes[0]) is GameClassInfo
 
 
 # todo: по факту зависит еще и от успешного прохождения
 #  тестов на создание модели - tests.models.test_caster
-@pytest.mark.dependency(name="save_caster", depends=["one_class"])
+@pytest.mark.dependency(scope="session",
+                        name="save_caster",
+                        depends=["tests/db/test_db_class.py::test_one_class"])
 def test_save_caster(full_db):
     data = {
         'name': 'Player1',
